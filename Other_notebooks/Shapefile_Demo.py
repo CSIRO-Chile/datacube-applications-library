2a938218b66d6f2f409494a718690ca01fc76ddf
gamedaygeorge <george@georgedyke.com>
2020-06-17 14:55:58 +1000

    Initia checkin of DCAL adapted version

---
+++ Other_notebooks/Shapefile_Demo.py
@@ -0,0 +1,56 @@
+# Code behind module for Shapefile_Demo.ipynb
+
+################################
+##
+## Import Statments
+##
+################################
+
+# Import standard Python modules
+import sys
+import datacube
+import numpy as np
+import fiona
+import xarray as xr 
+from rasterio.features import geometry_mask
+import shapely
+from shapely.ops import transform
+from shapely.geometry import shape
+from functools import partial
+import pyproj
+
+################################
+##
+## Function Definitions
+##
+################################
+
+def shapefile_mask(dataset: xr.Dataset, shapefile) -> np.array:
+    """Extracts a mask from a shapefile using dataset latitude and longitude extents.
+
+    Args:
+        dataset (xarray.Dataset): The dataset with the latitude and longitude extents.
+        shapefile (string): The shapefile to be used for extraction.
+
+    Returns:
+        A boolean mask array.
+    """
+    with fiona.open(shapefile, 'r') as source:
+        collection = list(source)
+        geometries = []
+        for feature in collection:
+            geom = shape(feature['geometry'])
+            project = partial(
+                pyproj.transform,
+                pyproj.Proj(init=source.crs['init']), # source crs
+                pyproj.Proj(init='epsg:4326')) # destination crs
+            geom = transform(project, geom)  # apply projection
+            geometries.append(geom)
+        geobox = dataset.geobox
+        mask = geometry_mask(
+            geometries,
+            out_shape=geobox.shape,
+            transform=geobox.affine,
+            all_touched=True,
+            invert=True)
+    return mask
